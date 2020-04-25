8b2501bd2bc89dbc9d8f711673c824e83f5ebdba
cctoemik <vvvins90nuar@gmail.com>
2020-04-25 05:23:45 +0300

    1

--- coupleapi/coupleapi/settings.py
+++ coupleapi/coupleapi/settings.py
@@ -80,22 +80,31 @@ WSGI_APPLICATION = 'coupleapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'coupleapi',
+#        'USER': 'tester',
+#        'HOST': 'localhost',
+#        'PASSWORD': 'tester!',
+#        'PORT': '5432',
+#        'OPTIONS': {
+#            'options': '-c search_path=public'
+#         }
+#     }
+#}
+
+#для контейнера
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'coupleapi',
-        'USER': 'toemik',
-        'HOST': 'localhost',
-        'PASSWORD': '55kjpddd!',
-        'PORT': '5432',
-        'OPTIONS': {
-            'options': '-c search_path=public'
-         }
-     }
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db', # set in docker-compose.yml
+        'PORT': 5432 # default postgres port
+    }
 }
 
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -113,8 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
+
 
 LANGUAGE_CODE = 'en-us'
 
@@ -126,9 +134,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 
